2db168f94c59916e5bff071a1a9cbcc9d21fb062
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-01-09 15:26:50 -0800

    README + Final Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Encryption Program (Python)
+An encryption program for messages or passwords. 
+
+## Description
+This program is built to encrypt and decrypt words/sentences by assigning a random symbol to each letter. This includes whitespaces, digits, punctuation, and letters. The User can then decrypt the message by entering the previously coded message.
+
+## Author
+Emmanuel Howard - https://github.com/Emmanuel-Howard
--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 # Encryption program that replaces passwords with random chars
 
+# Import random + string modules
 import random
 import string
 
+# Create chars with punctuation, digits, and letters
 chars = " " + string.punctuation + string.digits + string.ascii_letters
-chars = list(chars)
-key = chars.copy()
+chars = list(chars)   # Turn chars into a list
+key = chars.copy()    # Create a key (to shuffle the Encryption chars)
 
-random.shuffle(key)
+random.shuffle(key)   # Shuffle key so that the output is always different
 
-# print(f"{chars}")
-# print(f"{key}")
+# print(f"{chars}")   # Isn't needed in the program but useful to showcase
+# print(f"{key}")     # the difference in Chars vs Key (Shuffled)
 
-#Encrypt
+# ENCRYPTION
 plain_text = input("Enter a message to encrypt: ")
-cypher_text = ""
+cypher_text = ""   # Empty string for coded message
 
-for letter in plain_text:
-    index = chars.index(letter)
-    cypher_text += key[index]
+for letter in plain_text:          # For each letter in the User's input
+    index = chars.index(letter)    # Determine its position (index) in chars
+    cypher_text += key[index]      # Add that position (in key) to cypher_text (coded message)
 
-print(f"Encrypted Message: {cypher_text}")
+print(f"Encrypted Message: {cypher_text}")   # Prints coded message
 
-#DECRYPT
+# DECRYPTION
+# Same process as encryption but switch cypher_text / plain_text
 cypher_text = input("Enter a message to Decrypt: ")
 plain_text = " "
 
@@ -30,5 +33,5 @@ for letter in cypher_text:
     index = key.index(letter)
     plain_text += chars[index]
 
-print(f"Encrypted message: {cypher_text}")
-print(f"Original message: {plain_text}")
+print(f"Encrypted message: {cypher_text}")   # Prints coded message
+print(f"Original message: {plain_text}")     # Prints original message
